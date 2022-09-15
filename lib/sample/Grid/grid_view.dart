2754a39d196b1ace0a5141aa7736f47e6ac72072
ryuichi <ryuichi19633811@gmail.com>
2022-09-16 01:38:46 +0900

    add: スクロールの位置情報取得の機能

--- lib/sample/Grid/grid_view.dart
+++ lib/sample/Grid/grid_view.dart
@@ -13,22 +13,37 @@ class _GridViewSampleState extends State<GridViewSample> {
   // スクロールアニメーションの定義 & 初期化
   final ScrollController _scrollController = ScrollController();
 
+  // スクロールアニメーションの後処理
   @override
   void dispose() {
     _scrollController.dispose();
     super.dispose();
   }
 
+  // スクロール位置を取得する
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("グリッドレイアウト"),
+        title: Text(// スクロールビューとアタッチされていれば位置表記,アタッチしてなかったら例外処理として「0」を表記
+            "グリッドレイアウト　${_scrollController.hasClients ? _scrollController.position.pixels.toInt().toString() : 0} Pixels"),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
+          // グリッドを増やすボタン
           FloatingActionButton(
+            // 複数のFloatingActionButtonを利用するときは heroTag で認識する
             heroTag: "grid_on",
+            // 5列までは増やす
             onPressed: () {
               if (_columnsCount < 5) {
                 setState(() {
@@ -41,11 +56,14 @@ class _GridViewSampleState extends State<GridViewSample> {
               color: Colors.white,
             ),
           ),
+          // スペース
           const SizedBox(
             height: 16,
           ),
+          // グリッドを減らすボタン
           FloatingActionButton(
             heroTag: "grid_off",
+            // 1列までは減らす
             onPressed: () {
               if (_columnsCount > 1) {
                 setState(() {
@@ -61,9 +79,14 @@ class _GridViewSampleState extends State<GridViewSample> {
           const SizedBox(
             height: 16,
           ),
+          // トップに戻るボタン
           FloatingActionButton(
             heroTag: "arrow_upward",
             onPressed: () {
+              // アニメーション処理
+              // 第一引数：どこまで移動するか（今回は最上部の為、０）
+              // 第二引数： duration -> アニメーションの期間を指定
+              // 第三引数： curve -> アニメーションの推移の指定
               _scrollController.animateTo(0,
                   duration: const Duration(milliseconds: 600),
                   curve: Curves.easeInOutQuint);
@@ -82,12 +105,13 @@ class _GridViewSampleState extends State<GridViewSample> {
             crossAxisSpacing: 4,
             mainAxisSpacing: 4),
         itemBuilder: ((context, index) => Container(
-              color: Colors.teal,
+            color: Colors.teal,
+            child: Center(
               child: Text(
                 index.toString(),
-                style: const TextStyle(fontSize: 24, color: Colors.white),
+                style: const TextStyle(fontSize: 32, color: Colors.white),
               ),
-            )),
+            ))),
       ),
     );
   }
