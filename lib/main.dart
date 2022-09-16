02dd466c7e0c69c2b096f960b639073dd2dedbd5
ryuichi <ryuichi19633811@gmail.com>
2022-09-17 04:43:19 +0900

    change: sliverページに切り替え

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/sample/Grid/grid_view.dart';
+// import 'package:flutter_app/sample/Grid/grid_view.dart';
+import 'package:flutter_app/sample/Sliver/sliver_appbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const GridViewSample(),
+      // home: const GridViewSample(),
+      home: const SliverAppBarSample(),
     );
   }
 }
